dc2c9647e15a59e8f622c141c962ec7b34acc144
Prof J <benj1@umbc.edu>
2021-03-03 11:38:52 -0500

    plotting google trends data

---
+++ plot.py
@@ -0,0 +1,28 @@
+import csv
+
+import matplotlib
+import matplotlib.dates as mdates
+from matplotlib import pyplot
+
+with open('multiTimeline.csv', 'r') as f:
+    reader = csv.reader(f)
+
+    for i in range(2):
+        next(reader)
+
+    labels = list(next(reader))
+
+    data = []
+    x = []
+    for line in reader:
+        x.append(line[0]) # add the date to the x data
+        data.append(line[1:])
+
+dates = matplotlib.dates.date2num(x)
+
+for i in range(len(data[0])):
+    pyplot.plot_date(dates, [int(y[i]) for y in data], fmt='.-', label=labels[i+1])
+
+
+pyplot.legend()
+pyplot.show()
